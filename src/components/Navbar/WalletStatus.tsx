cbbab0cc7dc8996005d208630c9b40df58cf0a82
Vineet <vineet@altercode.xyz>
2021-11-10 00:15:57 +0530

    Updated stats of "Your Wallet"

--- src/components/Navbar/WalletStatus.tsx
+++ src/components/Navbar/WalletStatus.tsx
@@ -1,18 +1,38 @@
 import { Img } from '@chakra-ui/image';
-import { Box, Container, Flex, HStack, Text, VStack } from '@chakra-ui/layout';
-import React from 'react';
+import {
+  Box,
+  Container,
+  Flex,
+  HStack,
+  LinkBox,
+  Text,
+  VStack,
+} from '@chakra-ui/layout';
+import { LinkOverlay } from '@chakra-ui/react';
+import { BigNumber } from '@ethersproject/bignumber';
+import { CurrencyAmount } from '@uniswap/sdk-core';
+import NextLink from 'next/link';
+import React, { useMemo } from 'react';
 
 import { CMK, SCMK } from '~/constants/tokens';
+import { useAccessKeys } from '~/hooks/useAccessKeys';
+import {
+  useAccessKeyContract,
+  useStakedCredmarkContract,
+} from '~/hooks/useContract';
 import { useActiveWeb3React } from '~/hooks/web3';
-import { useSingleCallResult } from '~/state/multicall/hooks';
-
-import { useAccessKeyContract } from '../../hooks/useContract';
-import { useTokenBalance } from '../../state/wallet/hooks';
-import { formatTokenAmount } from '../../utils/formatTokenAmount';
+import {
+  Result,
+  useSingleCallResult,
+  useSingleContractMultipleData,
+} from '~/state/multicall/hooks';
+import { useTokenBalance } from '~/state/wallet/hooks';
+import { formatTokenAmount } from '~/utils/formatTokenAmount';
 
 export default function WalletStatus() {
   const { chainId, account } = useActiveWeb3React();
   const accessKeyContract = useAccessKeyContract();
+  const sCmkContract = useStakedCredmarkContract();
 
   const { loading: accessKeysResultLoading, result: accessKeysResult } =
     useSingleCallResult(accessKeyContract, 'balanceOf', [account ?? undefined]);
@@ -25,10 +45,33 @@ export default function WalletStatus() {
     chainId ? CMK[chainId] : undefined,
   );
 
-  const sCmkBalance = useTokenBalance(
-    account ?? undefined,
-    chainId ? SCMK[chainId] : undefined,
+  const { tokenIds } = useAccessKeys(account);
+
+  const result = useSingleContractMultipleData(
+    accessKeyContract,
+    'cmkValue',
+    (tokenIds ?? []).map((tokenId) => [tokenId]),
   );
+
+  const totalCmkValue = useMemo(() => {
+    return result
+      .map(({ result }) => result)
+      .filter((result): result is Result => !!result)
+      .map((result) => result[0] as BigNumber)
+      .reduce((prev, curr) => prev.add(curr), BigNumber.from(0));
+  }, [result]);
+
+  const { result: sCmkBalanceResult } = useSingleCallResult(
+    sCmkContract,
+    'cmkToShares',
+    [totalCmkValue],
+  );
+
+  const sCMK = chainId ? SCMK[chainId] : undefined;
+  const sCmkBalance =
+    sCmkBalanceResult && sCMK
+      ? CurrencyAmount.fromRawAmount(sCMK, sCmkBalanceResult?.[0].toString())
+      : undefined;
 
   return (
     <Container
@@ -49,22 +92,26 @@ export default function WalletStatus() {
       </Text>
       <Flex align="center" mt="8" mb="4">
         <HStack flex="1" spacing="8">
-          <VStack spacing="0">
+          <LinkBox as={VStack} spacing="0">
             <Img src="/img/key.png" h="10" />
             <Text fontWeight="bold" color="purple.500" lineHeight="1.2" pt="2">
               {accessKeysResultLoading || accessKeysMinted === undefined
                 ? '??'
                 : accessKeysMinted}
             </Text>
-            <Text
-              fontWeight="300"
-              color="purple.500"
-              lineHeight="1.2"
-              fontSize="sm"
-            >
-              Key Minted
-            </Text>
-          </VStack>
+            <NextLink href="/accessKeys" passHref>
+              <LinkOverlay>
+                <Text
+                  fontWeight="300"
+                  color="purple.500"
+                  lineHeight="1.2"
+                  fontSize="sm"
+                >
+                  Key Minted
+                </Text>
+              </LinkOverlay>
+            </NextLink>
+          </LinkBox>
           <Box w="2px" h="80px" bgGradient="linear(to-b, purple.500, white)" />
           <VStack spacing="0">
             <Img src="/img/cmk.png" h="10" />
