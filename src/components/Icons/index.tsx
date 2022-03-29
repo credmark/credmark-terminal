3d676df906c197bbef58a5c4bac39d5da191a823
Vineet <vineet@altercode.xyz>
2022-03-29 14:35:51 +0530

    feat: added cmk icons

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as CmkDashboardIcon } from './CmkDashboardIcon';
+export { default as CmkDataIcon } from './CmkDataIcon';
+export { default as CmkLogoIcon } from './CmkLogoIcon';
+export { default as CmkResearchIcon } from './CmkResearchIcon';
+export { default as CmkTerminalIcon } from './CmkTerminalIcon';
