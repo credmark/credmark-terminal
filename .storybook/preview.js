1e20b0891ef1a39d8d3200a57ba4adf79e6e8266
Robert Kibet <kipronokrobert@gmail.com>
2022-05-18 11:56:53 +0300

    feat: add chakra addons and configs plus enable absolute paths within stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
+import theme from '../src/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  chakra: {
+    theme: theme,
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
