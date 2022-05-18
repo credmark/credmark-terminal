b4f6935b6817d4af6b9a7e1ed52c7fdd4607d165
Robert Kibet <kipronokrobert@gmail.com>
2022-05-18 17:05:23 +0300

    fix: static image source for storybook

--- src/stories/ChartsHeader.stories.tsx
+++ src/stories/ChartsHeader.stories.tsx
@@ -1,7 +1,7 @@
+import { Img } from '@chakra-ui/react';
 import { Story } from '@storybook/react';
 import React from 'react';
 
-import wallet from '../../public/img/wallet.svg';
 import ChartHeaders, {
   ChartHeaderProps,
 } from '../components/shared/Charts/ChartHeader';
@@ -27,7 +27,7 @@ export default {
 const Template: Story<ChartHeaderProps> = (args) => (
   <ChartHeaders
     title="Chart Title"
-    logo={wallet}
+    logo={<Img src="https://app.credmark.com/img/logo.svg" width="20px" />}
     downloadFileName={`download-file-name.csv`}
     downloadFileHeaders={[]}
     downloadData={[]}
