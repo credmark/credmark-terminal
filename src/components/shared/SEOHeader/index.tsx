9b76822fd2d9ce50b5a35b1fac8a45b1b009c83e
Robert Kibet <kipronokrobert@gmail.com>
2022-05-24 14:37:53 +0300

    fix: seo head component change props

--- src/components/shared/SEOHeader/index.tsx
+++ src/components/shared/SEOHeader/index.tsx
@@ -4,24 +4,14 @@ import React from 'react';
 interface SEOHeaderProps {
   title?: string;
   description?: string;
-  canonical?: string;
+  url?: string;
 }
 const SEOHeader = ({
-  canonical = 'https://app.credmark.com/',
-  title = 'Credmark',
+  url = 'https://app.credmark.com/',
+  title = 'Credmark App',
   description = 'Credmark is a financial modeling platform powered by reliable on-chain data. Validated models are readily composable making rapid prototyping simple.',
 }: SEOHeaderProps) => {
-  return (
-    <NextSeo
-      title={title}
-      description={description}
-      canonical={canonical}
-      twitter={{
-        handle: '@credmarkhq',
-        site: '@credmarkhq',
-      }}
-    />
-  );
+  return <NextSeo title={title} description={description} canonical={url} />;
 };
 
 export default SEOHeader;
