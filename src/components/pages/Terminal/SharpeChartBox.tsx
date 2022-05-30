0eec0c7030c811a93e7c67ba9d6698029f5fbb6d
Vineet <vineet@altercode.xyz>
2022-05-30 21:20:21 +0530

    feat: sharpe ratio page

---
+++ src/components/pages/Terminal/SharpeChartBox.tsx
@@ -0,0 +1,371 @@
+import {
+  Box,
+  CloseButton,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  ListItem,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  UnorderedList,
+} from '@chakra-ui/react';
+import useSize from '@react-hook/size';
+import { Token } from '@uniswap/sdk-core';
+import { EChartsInstance } from 'echarts-for-react';
+import { DateTime, Duration } from 'luxon';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { MdAdd } from 'react-icons/md';
+
+import { BorderedCard } from '~/components/base';
+import ChartHeader from '~/components/shared/Charts/ChartHeader';
+import HistoricalChart from '~/components/shared/Charts/HistoricalChart';
+import CurrencyLogo from '~/components/shared/CurrencyLogo';
+import { useLineChart } from '~/hooks/useChart';
+import { useDeepCompareEffect } from '~/hooks/useDeepCompare';
+import {
+  ModelRunnerCallbackProps,
+  useModelRunnerCallback,
+} from '~/hooks/useModel';
+import { ModelSeriesOutput } from '~/types/model';
+
+interface TokenPrice {
+  price: number;
+  src?: string | undefined;
+}
+
+const colors = ['#00d696', '#00a1ed', '#8342af', '#ff447d', '#ffa727'];
+
+interface SharpeRatio {
+  token_address: string;
+  sharpe_ratio: number;
+  avg_return: number;
+  risk_free_rate: number;
+  ret_stdev: number;
+  return_rolling_interval: number;
+  blockTime: string;
+  block_number: number;
+  blockTimestamp: number;
+}
+
+function useSharpeRatioModel(tokens: string[]) {
+  const [pricesLoading, setPricesLoading] = useState(false);
+  const [sharpeLoading, setSharpeLoading] = useState(false);
+
+  const runPriceModel = useModelRunnerCallback<
+    unknown,
+    ModelSeriesOutput<TokenPrice>
+  >();
+
+  const runSharpeModel = useModelRunnerCallback<unknown, SharpeRatio>();
+
+  const [tokenPrices, setTokenPrices] = useState<
+    Record<string, ModelSeriesOutput<TokenPrice>>
+  >({});
+
+  const [sharpeRatios, setSharpeRatios] = useState<
+    ModelSeriesOutput<SharpeRatio>[]
+  >([]);
+
+  const PRICE_WINDOW = 30;
+  const SHARPE_WINDOW = 10;
+
+  useDeepCompareEffect(() => {
+    setPricesLoading(true);
+    const abortController = new AbortController();
+    const window = Duration.fromObject({ days: PRICE_WINDOW }).as('seconds');
+    const interval = Duration.fromObject({ days: 1 }).as('seconds');
+    const endTime = DateTime.now().startOf('day').toJSDate();
+    Promise.all(
+      tokens.map((tokenAddress) =>
+        tokenAddress in tokenPrices
+          ? Promise.resolve(tokenPrices[tokenAddress])
+          : runPriceModel(
+              {
+                slug: 'series.time-start-end-interval',
+                input: {
+                  modelSlug: 'chainlink.price-usd',
+                  modelInput: { address: tokenAddress },
+                  start: endTime.valueOf() / 1000 - window,
+                  end: endTime.valueOf() / 1000,
+                  interval,
+                },
+              },
+              abortController.signal,
+            ).then((result) => result.output),
+      ),
+    )
+      .then((resp) => {
+        const map: Record<string, ModelSeriesOutput<TokenPrice>> = {};
+        for (let i = 0; i < resp.length; i++) {
+          map[tokens[i]] = resp[i];
+        }
+
+        setTokenPrices(map);
+      })
+      .finally(() => {
+        setPricesLoading(false);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, [runPriceModel, tokens, tokenPrices]);
+
+  useDeepCompareEffect(() => {
+    setSharpeLoading(true);
+    const abortController = new AbortController();
+    function foo(
+      tokenAddress: string,
+      prices: ModelSeriesOutput<TokenPrice>,
+    ): Promise<ModelSeriesOutput<SharpeRatio>> {
+      return new Promise((resolve) => {
+        const series: ModelSeriesOutput<SharpeRatio>['series'] = [];
+        const errors: ModelSeriesOutput<SharpeRatio>['errors'] = [];
+        // Oldest first
+        const sortedPrices = [...prices.series].sort(
+          (a, b) => b.blockNumber - a.blockNumber,
+        );
+
+        const promises: Promise<unknown>[] = [];
+        for (let i = 0; i < SHARPE_WINDOW; i++) {
+          const inputPrices = sortedPrices.slice(
+            i,
+            sortedPrices.length - SHARPE_WINDOW + i,
+          );
+
+          const runProps: ModelRunnerCallbackProps<unknown> = {
+            slug: 'finance.sharpe-ratio-token',
+            blockNumber: inputPrices[0].blockNumber,
+            input: {
+              token: { address: tokenAddress },
+              prices: {
+                series: inputPrices,
+                errors: [],
+              },
+              risk_free_rate: 0.02,
+            },
+          };
+
+          promises.push(
+            runSharpeModel(runProps, abortController.signal)
+              .then((result) => {
+                series[i] = {
+                  blockNumber: inputPrices[0].blockNumber,
+                  blockTimestamp: inputPrices[0].blockTimestamp,
+                  sampleTimestamp: inputPrices[0].sampleTimestamp,
+                  output: result.output,
+                };
+              })
+              .catch((err) => {
+                errors[i] = {
+                  error: {
+                    message: err?.message ?? 'Some unexpected error occured',
+                  },
+                };
+              }),
+          );
+        }
+
+        Promise.all(promises).then(() => {
+          resolve({
+            series,
+            errors,
+          });
+        });
+      });
+    }
+
+    Promise.all(
+      Object.entries(tokenPrices).map(([tokenAddress, tokenPrice]) =>
+        foo(tokenAddress, tokenPrice),
+      ),
+    )
+      .then(setSharpeRatios)
+      .finally(() => {
+        setSharpeLoading(false);
+      });
+
+    return () => abortController.abort();
+  }, [runSharpeModel, tokenPrices]);
+
+  return { loading: pricesLoading || sharpeLoading, output: sharpeRatios };
+}
+
+interface SharpeChartBoxProps {
+  tokens: Token[];
+}
+
+export default function SharpeChartBox({ tokens }: SharpeChartBoxProps) {
+  const chartRef = useRef<EChartsInstance>();
+  const containerRef = useRef(null);
+
+  const [containerWidth] = useSize(containerRef);
+
+  const [selectedTokens, setSelectedTokens] = useState<string[]>(
+    tokens.slice(0, 3).map((token) => token.address),
+  );
+
+  useLayoutEffect(() => {
+    chartRef.current?.resize();
+  }, [containerWidth]);
+
+  const model = useSharpeRatioModel(selectedTokens);
+
+  const sharpeChart = useLineChart({
+    lines: model.output.map((o, i) => ({
+      color: colors[i],
+      name:
+        tokens.find((token) => token.address === selectedTokens[i])?.symbol ??
+        '',
+      data: o.series.map((i) => ({
+        timestamp: new Date(i.sampleTimestamp * 1000),
+        value: i.output.sharpe_ratio,
+      })),
+    })),
+    loading: model.loading,
+    error: '',
+    formatter: 'number',
+    fractionDigits: 2,
+  });
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const toggleFullScreen = () => {
+    if (!isFullScreen) {
+      document?.getElementById('sharpe-chart')?.requestFullscreen();
+      setIsFullScreen(true);
+    } else {
+      document?.exitFullscreen();
+      setIsFullScreen(false);
+    }
+  };
+
+  return (
+    <BorderedCard ref={containerRef} id="sharpe-chart">
+      <ChartHeader
+        title={'Sharpe Ratio'}
+        tooltip={
+          <Text>
+            Sharpe ratio is a traditional measure of risk-adjusted returns, that
+            allows the comparison of different assets and portfolios. In
+            traditional finance, the following decision making is applied when
+            using Sharpe ratio:
+            <UnorderedList>
+              <ListItem>Under 1.0 is considered bad</ListItem>
+              <ListItem>1.0 is considered acceptable or good</ListItem>
+              <ListItem>2.0 or higher is rated as very good</ListItem>
+            </UnorderedList>
+            <br />
+            <Link
+              href="https://docs.credmark.com/smart-money-in-defi/investment-concepts/sharpe-ratio-in-defi"
+              isExternal
+              textDecoration="underline"
+              pb="1"
+            >
+              Read more about Sharpe Ratio in Credmark Wiki{' '}
+              <Icon as={FaExternalLinkAlt} />
+            </Link>
+          </Text>
+        }
+        downloadFileName={`Sharpe Ratio [Credmark].csv`}
+        downloadFileHeaders={sharpeChart.csv.headers}
+        downloadData={sharpeChart.csv.data}
+        isFullScreen={isFullScreen}
+        toggleFullScreen={toggleFullScreen}
+      />
+      <Box overflowX="auto" py="2">
+        <HStack>
+          {selectedTokens.map((tokenAddress, index) => {
+            const token = tokens.find(
+              (token) => token.address === tokenAddress,
+            );
+            if (!token) throw Error('Invalid token address');
+
+            return (
+              <Box
+                textAlign="center"
+                px="4"
+                py="2"
+                key={token.address}
+                role="group"
+              >
+                <HStack w="100%" justifyContent="center">
+                  <CurrencyLogo currency={token} /> <Text>{token.symbol}</Text>{' '}
+                  <CloseButton
+                    isDisabled={model.loading}
+                    size="sm"
+                    opacity={0.5}
+                    _groupHover={{ opacity: 1 }}
+                    onClick={() =>
+                      setSelectedTokens(
+                        selectedTokens.filter(
+                          (tokenAddress) => token.address !== tokenAddress,
+                        ),
+                      )
+                    }
+                  />
+                </HStack>
+                <HStack w="100%" justifyContent="center">
+                  <Box w="4" h="4" rounded="full" bg={colors[index]} />
+                  <Text fontSize="2xl">
+                    {sharpeChart.currentStats?.[index]?.value ?? '-'}
+                  </Text>
+                </HStack>
+              </Box>
+            );
+          })}
+          {tokens.length > selectedTokens.length && selectedTokens.length < 5 && (
+            <Box textAlign="center">
+              <Text fontSize="xs" mb="2">
+                Add Token
+              </Text>
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  rounded="full"
+                  colorScheme="gray"
+                  icon={<Icon as={MdAdd} />}
+                ></MenuButton>
+                <MenuList maxH="300px" overflowY="auto">
+                  {tokens
+                    .filter((token) => !selectedTokens.includes(token.address))
+                    .sort((a, b) =>
+                      (a.symbol ?? '')?.localeCompare(b.symbol ?? ''),
+                    )
+                    .map((token) => (
+                      <MenuItem
+                        key={token.address}
+                        onClick={() => {
+                          setSelectedTokens([...selectedTokens, token.address]);
+                        }}
+                      >
+                        <HStack>
+                          <CurrencyLogo currency={token} />{' '}
+                          <Text>{token.symbol}</Text>
+                        </HStack>
+                      </MenuItem>
+                    ))}
+                </MenuList>
+              </Menu>
+            </Box>
+          )}
+        </HStack>
+      </Box>
+      <Flex align="stretch">
+        <HistoricalChart
+          height={600}
+          flex="1"
+          onChartReady={(chart) => (chartRef.current = chart)}
+          durations={[30, 60, 90]}
+          defaultDuration={30}
+          {...sharpeChart}
+        />
+      </Flex>
+    </BorderedCard>
+  );
+}
---
+++ src/pages/terminal/sharpe/index.tsx
@@ -0,0 +1,231 @@
+import { Container } from '@chakra-ui/react';
+import { Token, WETH9 } from '@uniswap/sdk-core';
+import React from 'react';
+
+import SharpeChartBox from '~/components/pages/Terminal/SharpeChartBox';
+import { DAI, USDC, UST, WBTC } from '~/constants/tokens';
+
+const APE = new Token(
+  1,
+  '0x4d224452801ACEd8B2F0aebE155379bb5D594381',
+  18,
+  'APE',
+  'ApeCoin',
+);
+const AAVE = new Token(
+  1,
+  '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
+  18,
+  'AAVE',
+  'Aave Token',
+);
+const COMP = new Token(
+  1,
+  '0xc00e94Cb662C3520282E6f5717214004A7f26888',
+  18,
+  'COMP',
+  'Compound',
+);
+const UNI = new Token(
+  1,
+  '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+  18,
+  'UNI',
+  'Uniswap',
+);
+const SUSHI = new Token(
+  1,
+  '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2',
+  18,
+  'SUSHI',
+  'SushiToken',
+);
+const USDT = new Token(
+  1,
+  '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+  6,
+  'USDT',
+  'Tether USD',
+);
+const CRV = new Token(
+  1,
+  '0xD533a949740bb3306d119CC777fa900bA034cd52',
+  18,
+  'CRV',
+  'Curve DAO Token',
+);
+const CVX = new Token(
+  1,
+  '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B',
+  18,
+  'CVX',
+  'Convex Token',
+);
+const FRAX = new Token(
+  1,
+  '0x853d955aCEf822Db058eb8505911ED77F175b99e',
+  18,
+  'FRAX',
+  'Frax',
+);
+const FXS = new Token(
+  1,
+  '0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0',
+  18,
+  'FXS',
+  'Frax Share',
+);
+const SHIB = new Token(
+  1,
+  '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
+  18,
+  'SHIB',
+  'SHIBA INU',
+);
+const MATIC = new Token(
+  1,
+  '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0',
+  18,
+  'MATIC',
+  'Matic Token',
+);
+const MKR = new Token(
+  1,
+  '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+  18,
+  'MKR',
+  'Maker',
+);
+const ONE_INCH = new Token(
+  1,
+  '0x111111111117dC0aa78b770fA6A738034120C302',
+  18,
+  '1INCH',
+  '1INCH Token',
+);
+const GNO = new Token(
+  1,
+  '0x6810e776880C02933D47DB1b9fc05908e5386b96',
+  18,
+  'GNO',
+  'Gnosis Token',
+);
+const LINK = new Token(
+  1,
+  '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  18,
+  'LINK',
+  'ChainLink Token',
+);
+const MANA = new Token(
+  1,
+  '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942',
+  18,
+  'MANA',
+  'Decentraland MANA',
+);
+const OHM = new Token(
+  1,
+  '0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5',
+  9,
+  'OHM',
+  'Olympus',
+);
+const RBN = new Token(
+  1,
+  '0x6123B0049F904d730dB3C36a31167D9d4121fA6B',
+  18,
+  'RBN',
+  'Ribbon',
+);
+const TRIBE = new Token(
+  1,
+  '0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B',
+  18,
+  'TRIBE',
+  'Tribe',
+);
+const TOKE = new Token(
+  1,
+  '0x2e9d63788249371f1DFC918a52f8d799F4a38C94',
+  18,
+  'TOKE',
+  'Tokemak',
+);
+const DYDX = new Token(
+  1,
+  '0x92D6C1e31e14520e676a687F0a93788B716BEff5',
+  18,
+  'DYDX',
+  'dYdX',
+);
+const ILV = new Token(
+  1,
+  '0x767FE9EDC9E0dF98E07454847909b5E959D7ca0E',
+  18,
+  'ILV',
+  'Illuvium',
+);
+const AXS = new Token(
+  1,
+  '0xBB0E17EF65F82Ab018d8EDd776e8DD940327B28b',
+  18,
+  'AXS',
+  'Axie Infinity Shard',
+);
+const AUDIO = new Token(
+  1,
+  '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998',
+  18,
+  'AUDIO',
+  'Audius',
+);
+const YFI = new Token(
+  1,
+  '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+  18,
+  'YFI',
+  'yearn.finance',
+);
+
+const tokens: Token[] = [
+  WBTC,
+  WETH9[1],
+  UST,
+  USDC[1],
+  DAI,
+  APE,
+  AAVE,
+  COMP,
+  UNI,
+  SUSHI,
+  USDT,
+  CRV,
+  CVX,
+  FRAX,
+  FXS,
+  SHIB,
+  MATIC,
+  MKR,
+  ONE_INCH,
+  GNO,
+  LINK,
+  MANA,
+  OHM,
+  RBN,
+  TRIBE,
+  TOKE,
+  DYDX,
+  ILV,
+  AXS,
+  AUDIO,
+  YFI,
+];
+
+export default function SharpeRatioPage() {
+  return (
+    <Container p="8" maxW="container.xl">
+      <SharpeChartBox tokens={tokens} />
+    </Container>
+  );
+}
