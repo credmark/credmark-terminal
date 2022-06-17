38f52b491507bcd7c162f638987205b645142691
Robert Kibet <kipronokrobert@gmail.com>
2022-06-17 12:54:51 +0300

    feat: fix storybook breaking after chart components update

--- src/stories/Charts.stories.tsx
+++ src/stories/Charts.stories.tsx
@@ -1,7 +1,7 @@
 import { Button, Grid } from '@chakra-ui/react';
 import React from 'react';
 
-import HistoricalChart from '../components/shared/Charts/HistoricalChart';
+import HistoricalChart from '~/components/shared/Charts/HistoricalChart';
 
 export default {
   title: 'Components/Charts/HistoricalChart',
@@ -11,6 +11,46 @@ export default {
   },
 };
 
+const lines = [
+  {
+    name: 'Historical Chart',
+    description: <p>Historicalchart</p>,
+    color: 'blue',
+    data: [
+      {
+        timestamp: new Date('2022-04-01T00:00:00.000Z'),
+        value: 100,
+      },
+      {
+        timestamp: new Date('2022-03-01T00:00:00.000Z'),
+        value: 900,
+      },
+      {
+        timestamp: new Date('2022-04-01T00:00:00.000Z'),
+        value: 322,
+      },
+    ],
+  },
+  {
+    name: 'Another one',
+    description: <p>Another one</p>,
+    color: 'red',
+    data: [
+      {
+        timestamp: new Date('2022-06-01T00:00:00.000Z'),
+        value: 100,
+      },
+      {
+        timestamp: new Date('2022-05-01T00:00:00.000Z'),
+        value: 300,
+      },
+      {
+        timestamp: new Date('2022-04-01T00:00:00.000Z'),
+        value: 876,
+      },
+    ],
+  },
+];
 const Template = () => {
   const [hasSidebar, showSidebar] = React.useState(false);
   return (
@@ -18,7 +58,16 @@ const Template = () => {
       <Button size="sm" onClick={() => showSidebar(!hasSidebar)}>
         Show/Hide Sidebar
       </Button>
-      <HistoricalChart lines={[]} />
+      <HistoricalChart
+        height={200}
+        flex="1"
+        borderLeft="2px"
+        borderColor="gray.100"
+        durations={[30, 60, 90]}
+        defaultDuration={30}
+        showCurrentStats
+        lines={lines}
+      />
     </Grid>
   );
 };
