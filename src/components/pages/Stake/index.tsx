ef7f806b26a4169c5d4f17edef672007a87090fa
Vineet <vineet@altercode.xyz>
2022-06-20 18:27:56 +0530

    feat: wallet status on stake page

--- src/components/pages/Stake/index.tsx
+++ src/components/pages/Stake/index.tsx
@@ -1,4 +1,5 @@
 export { default as GlobalStakeInfo } from './GlobalStakeInfo';
 export { default as StakePanel } from './StakePanel';
 export { default as UnstakePanel } from './UnstakePanel';
+export { default as WalletStatus } from './WalletStatus';
 export { default as XCmkFaq } from './XCmkFaq';
