5da1c0416ea1c5451ec266ba99f72c2c37e9cde7
Vineet <vineet@altercode.xyz>
2022-07-08 14:22:32 +0530

    fix: transpile echarts & zrender

--- next.config.js
+++ next.config.js
@@ -3,28 +3,31 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withTM = require('next-transpile-modules')(['echarts', 'zrender']);
 
-module.exports = withBundleAnalyzer({
-  async redirects() {
-    return [
-      {
-        source: '/analytics',
-        destination: '/info',
-        permanent: true,
-      },
-    ];
-  },
-  swcMinify: true,
-  webpack: (config) => {
-    // Suppressing abi JSON import warnings in @uniswap/v3-sdk/dist/v3-sdk.esm.js
-    // https://webpack.js.org/migrate/5/#using-named-exports-from-json-modules
-    config.ignoreWarnings = [
-      {
-        module: /.\/node_modules.*v3-sdk.*.js$/,
-        message: /only default export is available soon/,
-      },
-    ];
+module.exports = withTM(
+  withBundleAnalyzer({
+    async redirects() {
+      return [
+        {
+          source: '/analytics',
+          destination: '/info',
+          permanent: true,
+        },
+      ];
+    },
+    swcMinify: true,
+    webpack: (config) => {
+      // Suppressing abi JSON import warnings in @uniswap/v3-sdk/dist/v3-sdk.esm.js
+      // https://webpack.js.org/migrate/5/#using-named-exports-from-json-modules
+      config.ignoreWarnings = [
+        {
+          module: /.\/node_modules.*v3-sdk.*.js$/,
+          message: /only default export is available soon/,
+        },
+      ];
 
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
