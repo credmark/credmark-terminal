03e159e59c3be2f1e76997add55db300b10ee520
Vineet <vineet@altercode.xyz>
2022-07-19 22:35:16 +0530

    fix: added frax links in sidebar

--- src/pages/terminal/stablecoin/frax-balances.tsx
+++ src/pages/terminal/stablecoin/frax-balances.tsx
@@ -6,10 +6,10 @@ import SEOHeader from '~/components/shared/SEOHeader';
 export default function DashboardPage() {
   return (
     <>
-      <SEOHeader title="FRAX Stats & Collateralization" />
+      <SEOHeader title="FRAX Balances & Holders" />
       <AspectRatio maxW="100vw" ratio={{ base: 1, lg: 4 / 3, xl: 16 / 9 }}>
         <iframe
-          title="FRAX Stats & Collateralization"
+          title="FRAX Balances & Holders"
           width="600"
           height="373.5"
           src="https://app.powerbi.com/view?r=eyJrIjoiNzFmMjNmZDYtY2VjYy00NDcyLTgxOTUtNDc1ZGFiODAzOTk1IiwidCI6ImZlMjVkZDFmLTFjODktNDBhYi04ODdiLWQzYmY2ZjA2MjI5MyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionfe99243aa16623f527c0"
