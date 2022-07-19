729b71a441ce4b38d00446a87126bdea7f4bad66
Vineet <vineet@altercode.xyz>
2022-07-19 22:14:54 +0530

    fix: primary button colors

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -3,6 +3,7 @@ import { Button, chakra } from '@chakra-ui/react';
 export const PrimaryButton = chakra(Button, {
   baseStyle: {
     colorScheme: 'green',
-    color: 'purple.500',
+    color: 'purple.800',
+    bg: 'green.500',
   },
 });
