bdd26b0a29185ecc6f1a666208bf6f87b09fde0d
Vineet <vineet@altercode.xyz>
2022-08-29 12:00:52 +0530

    fix: new logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,6 @@ class MyDocument extends Document {
             href="/apple-touch-icon.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" />
 
           {/* Google analytics integration */}
