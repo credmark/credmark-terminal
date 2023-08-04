ebda61cdb7807c519430dafcce60cd3a162299ce
v1nvn <v1n@outlook.com>
2023-08-04 17:10:36 +0530

    fix: currency logo sources

--- src/components/shared/CurrencyLogo/index.tsx
+++ src/components/shared/CurrencyLogo/index.tsx
@@ -7,6 +7,7 @@ import {
   ExtendedToken,
   NativeCurrency,
 } from '~/utils/currency';
+import isAddress from '~/utils/isAddress';
 
 export enum ChainId {
   ETHEREUM = 1,
@@ -45,49 +46,89 @@ export enum ChainId {
   SOLANA = 101,
 }
 
-const BLOCKCHAIN: Record<number, string> = {
-  [ChainId.ETHEREUM]: 'ethereum',
-  [ChainId.BSC]: 'binance',
-  [ChainId.CELO]: 'celo',
-  [ChainId.FANTOM]: 'fantom',
-  [ChainId.AVALANCHE_TESTNET]: 'fuji',
-  [ChainId.FUSE]: 'fuse',
-  [ChainId.HARMONY]: 'harmony',
-  [ChainId.HECO]: 'heco',
-  [ChainId.MATIC]: 'matic',
-  [ChainId.MOONRIVER]: 'moonriver',
-  [ChainId.OKEX]: 'okex',
-  [ChainId.PALM]: 'palm',
-  [ChainId.TELOS]: 'telos',
-  [ChainId.XDAI]: 'xdai',
-  [ChainId.ARBITRUM]: 'arbitrum',
-  [ChainId.AVALANCHE]: 'avalanchec',
-  [ChainId.MOONBEAM]: 'moonbeam',
-  [ChainId.HARDHAT]: 'hardhat',
-  [ChainId.SOLANA]: 'solana',
+const logos: Record<string, Record<number, string>> = {
+  trustwallet_assets: {
+    [ChainId.ETHEREUM]: 'ethereum',
+    [ChainId.BSC]: 'binance',
+    [ChainId.MATIC]: 'polygon',
+    [ChainId.OPTIMISM]: 'optimism',
+    [ChainId.ARBITRUM]: 'arbitrum',
+    [ChainId.AVALANCHE]: 'avalanchec',
+    [ChainId.FANTOM]: 'fantom',
+  },
+  uniswap_assets: {
+    [ChainId.ETHEREUM]: 'ethereum',
+    [ChainId.BSC]: 'binance',
+    [ChainId.MATIC]: 'polygon',
+    [ChainId.OPTIMISM]: 'optimism',
+    [ChainId.ARBITRUM]: 'arbitrum',
+    [ChainId.AVALANCHE]: 'avalanchec',
+    [ChainId.FANTOM]: 'fantom',
+  },
+  sushiswap_assets: {
+    [ChainId.ETHEREUM]: 'ethereum',
+    [ChainId.BSC]: 'binance',
+    [ChainId.MATIC]: 'polygon',
+    [ChainId.OPTIMISM]: 'optimism',
+    [ChainId.ARBITRUM]: 'arbitrum',
+    [ChainId.AVALANCHE]: 'avalanche',
+    [ChainId.FANTOM]: 'fantom',
+  },
+  sushiswap_logos: {
+    [ChainId.ETHEREUM]: 'ethereum',
+    [ChainId.BSC]: 'binance',
+    [ChainId.MATIC]: 'polygon',
+    [ChainId.OPTIMISM]: 'optimism',
+    [ChainId.ARBITRUM]: 'arbitrum',
+    [ChainId.AVALANCHE]: 'avalanche',
+    [ChainId.FANTOM]: 'fantom',
+  },
+  curve_assets: {
+    [ChainId.ETHEREUM]: 'assets',
+    [ChainId.MATIC]: 'assets-polygon',
+    [ChainId.OPTIMISM]: 'assets-optimism',
+    [ChainId.ARBITRUM]: 'assets-arbitrum',
+    [ChainId.AVALANCHE]: 'assets-avalanche',
+    [ChainId.FANTOM]: 'assets-fantom',
+  },
 };
 
 export const getTokenLogoURLs = (currency: ExtendedToken): string[] => {
   const { chainId, address, id } = currency;
-  const asset = id ?? address;
+  const asset = id ?? isAddress(address);
   const urls: string[] = [];
-  if (chainId === ChainId.ETHEREUM) {
+
+  if (!asset) {
+    return urls;
+  }
+
+  if (chainId in logos['trustwallet_assets']) {
     urls.push(
-      `https://raw.githubusercontent.com/uniswap/assets/master/blockchains/ethereum/assets/${asset}/logo.png`,
+      `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${logos['trustwallet_assets'][chainId]}/assets/${asset}/logo.png`,
     );
   }
 
-  if (chainId in BLOCKCHAIN) {
+  if (chainId in logos['uniswap_assets']) {
     urls.push(
-      `https://raw.githubusercontent.com/sushiswap/logos/main/network/${BLOCKCHAIN[chainId]}/${asset}.jpg`,
-      `https://raw.githubusercontent.com/sushiswap/assets/master/blockchains/${BLOCKCHAIN[chainId]}/assets/${asset}/logo.png`,
-      `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${BLOCKCHAIN[chainId]}/assets/${asset}/logo.png`,
+      `https://raw.githubusercontent.com/uniswap/assets/master/blockchains/${logos['uniswap_assets'][chainId]}/assets/${asset}/logo.png`,
     );
   }
 
-  if (chainId === ChainId.ETHEREUM) {
+  if (chainId in logos['sushiswap_logos']) {
     urls.push(
-      `https://raw.githubusercontent.com/curvefi/curve-assets/main/images/assets/${asset.toLowerCase()}.png`,
+      `https://raw.githubusercontent.com/sushiswap/list/master/logos/token-logos/network/${logos['sushiswap_logos'][chainId]}/${asset}.jpg`,
+    );
+  }
+
+  if (chainId in logos['sushiswap_assets']) {
+    urls.push(
+      `https://raw.githubusercontent.com/sushiswap/assets/master/blockchains/${logos['sushiswap_assets'][chainId]}/assets/${asset}/logo.png`,
+    );
+  }
+
+  if (chainId in logos['curve_assets']) {
+    urls.push(
+      `https://raw.githubusercontent.com/curvefi/curve-assets/main/images/${logos['curve_assets'][chainId]}/{input.address}.png`,
     );
   }
 
@@ -97,9 +138,9 @@ export const getTokenLogoURLs = (currency: ExtendedToken): string[] => {
 export const getNativeTokenLogoURLs = (currency: NativeCurrency): string[] => {
   const { chainId } = currency;
   const urls: string[] = [];
-  if (chainId in BLOCKCHAIN) {
+  if (chainId in logos.trustwallet_assets) {
     urls.push(
-      `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${BLOCKCHAIN[chainId]}/info/logo.png`,
+      `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${logos.trustwallet_assets[chainId]}/info/logo.png`,
     );
   }
 
@@ -143,6 +184,19 @@ export default function CurrencyLogo({
       <Image
         src="/img/cmk.svg"
         alt="CMK"
+        w={w}
+        h={h}
+        rounded="full"
+        {...boxProps}
+      />
+    );
+  }
+
+  if (currency?.symbol === 'ICHI') {
+    return (
+      <Image
+        src="/img/ichi.png"
+        alt="ICHI"
         w={w}
         h={h}
         rounded="full"
